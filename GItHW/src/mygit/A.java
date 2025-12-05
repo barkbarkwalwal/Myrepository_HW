2189a8bf9f1cbc85dcfcfb581982cd546293e8bf
songjy601 <songjy601@naver.com>
2025-12-05 23:07:09 +0900

    Alice에서 A, C 클래스 변경

--- GItHW/src/mygit/A.java
+++ GItHW/src/mygit/A.java
@@ -1,5 +1,11 @@
 package mygit;
 
 public class A {
-
+	public static void main(String[] args) {
+		System.out.println("Alice에서 A class 변경");
+		int a = 10;
+		for (int i = 1; i<10; i++) {
+			a+=i;
+		}
+	}
 }
--- GItHW/src/mygit/C.java
+++ GItHW/src/mygit/C.java
@@ -2,7 +2,7 @@ package mygit;
 
 public class C {
 	public static void main(String[] args) {
-		
+		System.out.println("Alice에서 C class 변경");
 
 	}
 }
